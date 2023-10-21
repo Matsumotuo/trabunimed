fa1da1a612e8eaa5e74e4740dea510757d015afa
Matsumotuo <zmatsu000@gmail.com>
2023-10-21 12:38:01 -0300

    adicionado o reset da senha

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ function openPagina(){
     window.open('telasistema.HTML','_self');
 }
 
+//data na pagina de cadastrar email
 document.addEventListener("load", function() {
+    //data na pagina de cadastrar email
     var dataLocal = new Date();
     var mes = dataLocal.getMonth() + 1;
 
@@ -17,3 +19,18 @@ document.addEventListener("load", function() {
     // Definindo a data mínima
     meucampo.setAttribute("min", dataMinima);
 });
+
+
+function redefinirsenha() {
+    const novasenha = document.getElementById('novasenha').value;
+    const confirmarsenha = document.getElementById('confirmarsenha').value;
+
+    if (novasenha !== confirmarsenha) {
+        alert('As senhas não são iguais, tente novamente ;)');
+        return;
+    }
+
+    console.log(`Nova senha: ${novasenha}`);
+    alert('Nova senha efetuada');
+    window.open('login.html','_self');
+}
